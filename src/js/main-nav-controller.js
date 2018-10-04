5b3342e216edf07b3a8f81a7059499cb88bc9847
John Boyer <joeRob2468@yahoo.com>
2018-10-04 11:44:36 -0500

    added nav bar color change when over dark elements

--- src/js/main-nav-controller.js
+++ src/js/main-nav-controller.js
@@ -1,4 +1,5 @@
 $(function() {
+  /* -- Overlay Toggle -- */
   let button = $('button.nav-overlay-toggle');
   let navOverlay = $('.nav-overlay');
   let activeClass = 'is-active';
@@ -19,5 +20,26 @@ $(function() {
 
   navOverlayItems.on('mouseout', function() {
     navOverlayBackgroundItems.filter('.' + $(this).data('target')).removeClass(activeClass);
-  })
+  });
+});
+
+$(function() {
+  /* -- Color Change in Dark Sections -- */
+  let navbar = $('nav.main-nav');
+  let darkElements = $('.section.is-dark:not(.nav-overlay), .section.is-grey:not(.nav-overlay)');
+   
+  darkElements.each(function(index) {
+    let elementWatcher = scrollMonitor.create($(this).get(0));
+
+    elementWatcher.stateChange(function() {
+      navbar.toggleClass('is-light', this.isInViewport && this.isAboveViewport || $(document).scrollTop() === this.top);
+    });
+
+    // if this is the first element, check if we're overlapping on page load
+    if (index === 0) {
+      elementWatcher.update();
+      navbar.toggleClass('is-light', elementWatcher.isInViewport && elementWatcher.isAboveViewport || $(document).scrollTop() == elementWatcher.top);
+    }
+  });
+
 });
